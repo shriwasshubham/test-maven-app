b81e1a2532fad537b354068ec0b507b26889374a
shriwasshubham <61614458+shriwasshubham@users.noreply.github.com>
2020-03-01 16:04:39 +0530

    Update Hello.java

--- src/main/java/com/example/hello/Hello.java
+++ src/main/java/com/example/hello/Hello.java
@@ -5,7 +5,6 @@ public class Hello {
 	public static void main(String [] args) {
 		System.out.println("Hello, world! I am here in Jenkins");
 		System.out.println("Hello, world! I am here in git");
-		System.out.println("Hello, world! I am here in git");
 	}
 	
 }
